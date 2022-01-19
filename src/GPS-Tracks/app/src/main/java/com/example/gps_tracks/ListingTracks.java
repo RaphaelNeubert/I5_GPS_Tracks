10aefed78abb25e50e1f92c588ba43edfafdd148
Alex Schechtel <alex.schechtel@htw-dresden.de>
2022-01-19 19:07:18 +0100

    Button oben links funktion

---
+++ src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/ListingTracks.java
@@ -0,0 +1,14 @@
+package com.example.gps_tracks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListingTracks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listing_tracks);
+    }
+}
