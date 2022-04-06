d9598781b36c26d2945194c873ecd72cd77a2ecc
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2022-04-06 20:29:32 +0200

    Befassung mit dem Dateisystem. #83

--- src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/ListingTracks.java
+++ src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/ListingTracks.java
@@ -1,5 +1,6 @@
 package com.example.gps_tracks;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,24 +16,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 
 public class ListingTracks extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     private Button back;
     String[] testdata = {"Neuer Track 05.11.2022 12:15 Uhr","Neuer Track 22.08.2022 18:25 Uhr",
                          "Runde durch den Park","Neuer Track 20.04.2022 03:43 Uhr",
                          "Neuer Track 20.04.2022 10:14 Uhr"};
+
+    //String[] instrumentFileList = ListingTracks.this.fileList();
     ListView listView;
     View callingItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         //will hide the title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // hide the title bar
+
+        // hide the title bars
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_listing_tracks);
-
 
         back = (Button) findViewById(R.id.backToHome);
         back.setOnClickListener(new View.OnClickListener() {
@@ -44,13 +49,22 @@ public class ListingTracks extends AppCompatActivity {
 
         //listing
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_list_view, testdata);
+                R.layout.activity_list_view, fileList());
 
         listView = (ListView) findViewById(R.id.listTracks);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // to do: delete if not required
+                Context context = getApplicationContext();
+                String[] files = context.fileList();
+                Log.i(TAG, String.valueOf(files.length));
+                for (int i = 0; i < files.length; i++) {
+                    Log.i(TAG, "file was found!");
+                    Log.i(TAG, files[i]);
+                }
                 String selectedItem = (String) parent.getItemAtPosition(position);
                 callingItem = view;
                 Log.i("button:", selectedItem);
