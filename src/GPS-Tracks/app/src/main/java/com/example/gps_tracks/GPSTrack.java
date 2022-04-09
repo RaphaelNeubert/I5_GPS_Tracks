06475c328bc7276164e84726a70e7625860ca795
RaphaelNeubert <raphi.neubert@gmail.com>
2022-04-09 13:30:44 +0200

    closes #88

--- src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/GPSTrack.java
+++ src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/GPSTrack.java
@@ -5,15 +5,20 @@ import android.location.Location;
 import android.util.Log;
 
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.IMyLocationConsumer;
 import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.ticofab.androidgpxparser.parser.GPXParser;
 
 public class GPSTrack {
     public enum State {READY, RECORDING, EDITING, EMPTY}
-    Context context;
+    private Context context;
     private GpsMyLocationProvider gpsMyLocationProvider;
     private State state = State.EMPTY;
 
@@ -21,9 +26,13 @@ public class GPSTrack {
     GPSTrack(Context context) {
         this.context= context;
     }
-    boolean startRecording() {
+    boolean startRecording(MapView map) {
         Log.i("GPSTrack","Recording has been started.");
         state = State.RECORDING;
+        int i = 0;
+        Polyline path = new Polyline();
+        map.getOverlayManager().add(path);
+
         gpsMyLocationProvider = new GpsMyLocationProvider(context);
         gpsMyLocationProvider.setLocationUpdateMinDistance(5.0f);
         gpsMyLocationProvider.startLocationProvider(new IMyLocationConsumer() {
@@ -31,6 +40,12 @@ public class GPSTrack {
             public void onLocationChanged(Location location, IMyLocationProvider source) {
                 String position = "Location changed to: " + new GeoPoint(location).toDoubleString();
                 Log.i("onLocationChanged: ", position);
+                path.addPoint(new GeoPoint(location));
+                //save current path on disk
+                if (i%10 == 0) {
+                    List<GeoPoint> points = path.getActualPoints();
+                    //TODO save List on disk as GPX
+                }
             }
         });
         return true;
