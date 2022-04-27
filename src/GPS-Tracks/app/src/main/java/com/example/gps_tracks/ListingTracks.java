fa4eff6d4716bccf1866cf44cfbd755324259e56
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2022-04-27 20:22:29 +0200

    #96 Synchronisation (sync fkt, button, anim)

--- src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/ListingTracks.java
+++ src/GPS-Tracks/app/src/main/java/com/example/gps_tracks/ListingTracks.java
@@ -3,7 +3,6 @@ package com.example.gps_tracks;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -13,11 +12,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -31,12 +36,12 @@ import okhttp3.Response;
 
 public class ListingTracks extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-
-    private Button back;
+    private Button back, sync;
+    ProgressBar progressBar;
     String[] testdata = {"Neuer Track 05.11.2022 12:15 Uhr","Neuer Track 22.08.2022 18:25 Uhr",
                          "Runde durch den Park","Neuer Track 20.04.2022 03:43 Uhr",
                          "Neuer Track 20.04.2022 10:14 Uhr"};
-
+    String fileList;
     //String[] instrumentFileList = ListingTracks.this.fileList();
     ListView listView;
     View callingItem;
@@ -44,34 +49,6 @@ public class ListingTracks extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-/*
-        // new File
-        String filename = "myfile.txt";
-        String string = "Hello world!";
-        FileOutputStream outputStream;
-        try {
-            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-            outputStream.write(string.getBytes());
-            outputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-*/
-
-        File dir = getFilesDir();
-        File file = new File(dir, "myfile.txt");
-
-        // deploy on http://pythonanywhere.com
-        //uploadFile("http://aleksandrpronin.pythonanywhere.com/upload",file);
-
-        try {
-            downloadFile("myfile.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
 
         //will hide the title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -89,7 +66,21 @@ public class ListingTracks extends AppCompatActivity {
             }
         });
 
-        //listing
+        sync = (Button)findViewById(R.id.sync);
+        sync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    synchronisation();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        // listing
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 R.layout.activity_list_view, fileList());
 
@@ -161,8 +152,9 @@ public class ListingTracks extends AppCompatActivity {
         p.show();
     }
 
-    public static Boolean uploadFile(String serverURL, File file) {
+    public static Boolean uploadFile(File file) {
         OkHttpClient okHttpClient = new OkHttpClient();
+        // deploy on http://pythonanywhere.com
 
         try {
             RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
@@ -171,7 +163,7 @@ public class ListingTracks extends AppCompatActivity {
                     .build();
 
             Request request = new Request.Builder()
-                    .url(serverURL)
+                    .url("http://aleksandrpronin.pythonanywhere.com/upload")
                     .post(requestBody)
                     .build();
 
@@ -181,7 +173,6 @@ public class ListingTracks extends AppCompatActivity {
                 public void onFailure(final Call call, final IOException e) {
                     // Handle the error
                     System.out.println("Error => " + e);
-
                 }
 
                 @Override
@@ -192,7 +183,6 @@ public class ListingTracks extends AppCompatActivity {
                     // Upload successful
                 }
             });
-
             return true;
         } catch (Exception ex) {
             // Handle the error
@@ -202,7 +192,6 @@ public class ListingTracks extends AppCompatActivity {
 
     public void downloadFile(String filename) throws IOException {
         OkHttpClient client = new OkHttpClient();
-
 
         Request request = new Request.Builder()
                 //.url("http://ip:5000/download/"+filename)
@@ -217,7 +206,7 @@ public class ListingTracks extends AppCompatActivity {
 
             @Override
             public void onResponse(final Call call, Response response) throws IOException {
-                Log.d("TAG",response.body().string());
+                //Log.d("TAG",response.body().string());
                 if (!response.isSuccessful()) {
                     throw new IOException("Failed to download file: " + response);
                 }
@@ -228,6 +217,76 @@ public class ListingTracks extends AppCompatActivity {
                     outputStream.close();
                 } catch (Exception e) {
                     e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void synchronisation() throws IOException {
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://aleksandrpronin.pythonanywhere.com/liste")
+                .build();
+
+         client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(final Call call, Response response) throws IOException {
+                String responseData = response.body().string();
+                String listFormated=responseData.replace("'", "").replace(" ","").replace("[","").replace("]","");
+                String[] arrayFromFlask = listFormated.split(",");
+
+                // сdownload
+                int count=1;
+                Context context = getApplicationContext();
+                String[] fileList = context.fileList();
+
+                List<String> filesListFromFlask = new ArrayList<>(Arrays.asList(arrayFromFlask));
+                System.out.println("Files From Flask: "+ filesListFromFlask);
+
+                for (int i = 0; i < fileList.length; i++) {
+                    filesListFromFlask.remove(fileList[i]);
+                    //listFormated=filesListFromFlask[i]
+                    if (i ==(fileList.length -1 )){
+                        finish();
+                        overridePendingTransition(10, 10);
+                        startActivity(getIntent());
+                        overridePendingTransition(10, 10);
+                    }
+                    if (filesListFromFlask.size() == 0){
+                        finish();
+                        overridePendingTransition(10, 10);
+                        startActivity(getIntent());
+                        overridePendingTransition(10, 10);
+                    }
+                }
+
+                for(String el:filesListFromFlask){
+                    System.out.println("Download from Flask: "+ filesListFromFlask);
+                    downloadFile(el);
+                }
+
+
+                // upload
+                File dir = getFilesDir();
+                File file;
+                List<String> localeFiles = new ArrayList<>(Arrays.asList(fileList));
+                localeFiles.remove("osmdroid");
+                System.out.println("Files Local: "+ localeFiles);
+
+                for (int i = 0; i < arrayFromFlask.length; i++) {
+                    localeFiles.remove(arrayFromFlask[i]);
+                }
+
+                for(String el:localeFiles){
+                    file = new File(dir, el);
+                    uploadFile(file);
+                    System.out.println("Locale Files zum upload "+ localeFiles);
                 }
             }
         });
